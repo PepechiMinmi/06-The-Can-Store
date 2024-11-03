6da89b67a69c2a937e45d92f65fa255b4722334d
PepechiMinmi <p5420077@stu.chs.nihon-u.ac.jp>
2024-11-03 22:51:51 +0900

    修正1:画像パス

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,7 @@ export default function App() {
                 <section className={product.type} key={index}>
                   <h2>{product.name}</h2>
                   <p>${product.price.toFixed(2)}</p>
-                  <img src={`public/images/${product.image}`} alt={product.name}//画像パス出来た
+                  <img src={`${process.env.PUBLIC_URL}/images/${product.image}`} alt={product.name}
                   />
                 </section>
                 ))
